8b032b6aec8bf8393133487fab9e2e2c53a1f703
daal2quf <daal2quf@gmail.com>
2022-12-04 22:32:28 +0900

    feat(OutputView): 게임 시작 문구 출력 기능 추가
    add:
    printGameStartMessage(): 게임 시작 문구 출력

---
+++ src/OutputView.js
@@ -0,0 +1,13 @@
+const { Console } = require('@woowacourse/mission-utils');
+
+const OutputView = {
+  printGuessResult() {},
+
+  printFinalResult() {},
+
+  printGameStartMessage() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+  },
+};
+
+module.exports = { OutputView };
