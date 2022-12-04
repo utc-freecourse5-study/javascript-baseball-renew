85ebbdc859d79538887f20cd4dcbf9bcf982a878
daal2quf <daal2quf@gmail.com>
2022-12-04 22:31:43 +0900

    feat(App): 게임 시작 기능 추가
    add:
    this.baseBallGame: App 클래스의 멤버변수로 설정함으로써 app 인스턴스가 생성시에 BaseBallGame의 인스턴스가 생성되도록 설정
    gameStart(): 야구게임의 정답을 컴퓨터숫자로 할당 및 게임 시작 메세지 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+const BaseBallGame = require('./BaseBallGame');
+const { OutputView } = require('./OutputView');
+
 class App {
-  play() {}
+  constructor() {
+    this.baseBallGame = new BaseBallGame();
+  }
+
+  play() {
+    this.gameStart();
+  }
+
+  gameStart() {
+    OutputView.printGameStartMessage();
+    this.baseBallGame.assignComputerNumbers();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
