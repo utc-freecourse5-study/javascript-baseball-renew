02fc0fe7e65b5f24f662c80e402ad58aec9eaf1a
turtle601 <turtle601@naver.com>
2022-12-04 21:25:18 +0900

    test(validationTest) BaseBallInput 테스트 작성

--- __tests__/validationTest.js
+++ __tests__/validationTest.js
@@ -1,4 +1,5 @@
 const GameCommandException = require('../src/validation/GameCommandException');
+const BaseBallInput = require('../src/validation/BaseBallInput');
 
 describe('GameCommand 입력값 테스트', () => {
   test.each([['a'], ['3'], ['21'], ['1 ']])(
@@ -14,5 +15,22 @@ describe('GameCommand 입력값 테스트', () => {
     const gameCommandException = new GameCommandException(input);
 
     expect(() => gameCommandException.validate()).not.toThrow();
+  });
+});
+
+describe('BaseBallInput 입력값 테스트', () => {
+  test.each([['13'], ['1a2'], ['123 '], ['122'], ['12 ']])(
+    '(실패) 숫자 입력값 확인',
+    (input) => {
+      const baseBallInput = new BaseBallInput(input);
+      expect(() => baseBallInput.validate()).toThrow('[ERROR]');
+    }
+  );
+
+  test('(성공) 재시작/종료 입력값 확인', () => {
+    const input = '123';
+    const baseBallInput = new BaseBallInput(input);
+
+    expect(() => baseBallInput.validate()).not.toThrow();
   });
 });
