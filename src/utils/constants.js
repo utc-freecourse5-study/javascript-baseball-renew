6329a1e7ce673cef981d0da61a2b55e7fe242919
turtle601 <turtle601@naver.com>
2022-12-05 01:00:24 +0900

    feat(constant) BASEBALL_TEXT 객체 추가

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -13,8 +13,15 @@ const MODEL_KEY = Object.freeze({
   trial: 'BASEBALL_TRY_COUNT',
 });
 
+const BASEBALL_TEXT = Object.freeze({
+  baseballInput: '숫자를 입력하세요 : ',
+  gameCommand: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
+  final: '3개의 숫자를 모두 맞히셨습니다! 게임 종료\n',
+});
+
 module.exports = {
   REGEX,
   ERROR_MESSAGE,
   MODEL_KEY,
+  BASEBALL_TEXT,
 };
