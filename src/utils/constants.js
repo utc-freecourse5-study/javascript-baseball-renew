06a7f47f37d956eaadcb8ba0eea6bd92f6c62f9e
turtle601 <turtle601@naver.com>
2022-12-04 21:31:46 +0900

    feat(constant) BaseballInputException 변수 선언
    Add:
    1) ERROR_MESSAGE
    2) REGEX

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,11 @@
 const REGEX = Object.freeze({
   gameCommand: /^1{1}$|^2{1}$/,
+  baseballInput: /^[1-9]{3}$/,
 });
 
 const ERROR_MESSAGE = Object.freeze({
   gameCommand: '[ERROR] 재시작/종료 명령어는 각각 1, 2이어야만 합니다.',
+  baseballInput: '[ERROR] [ERROR]: 서로 다른 세자리 숫자를 입력해주세요',
 });
 
 module.exports = {
