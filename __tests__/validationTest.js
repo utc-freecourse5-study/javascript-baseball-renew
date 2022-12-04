588a117ccf6d4d5674179679309d7537d908fb54
turtle601 <turtle601@naver.com>
2022-12-04 21:19:56 +0900

    test(validateTest) 입력값 수정

--- __tests__/validationTest.js
+++ __tests__/validationTest.js
@@ -1,5 +1,7 @@
+const GameCommandException = require('../src/validation/GameCommandException');
+
 describe('GameCommand 입력값 테스트', () => {
-  test.each([['a'], ['1'], ['21'], ['1 ']])(
+  test.each([['a'], ['3'], ['21'], ['1 ']])(
     '(실패) 재시작/종료 입력값 확인',
     (input) => {
       const gameCommandException = new GameCommandException(input);
