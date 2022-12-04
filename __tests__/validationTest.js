1e5883d6d3154ea47f5f1b1c5463b07291ce9cc1
turtle601 <turtle601@naver.com>
2022-12-04 21:09:16 +0900

    test(GameCommandException) 입력값 에러 테스트 작성

---
+++ __tests__/validationTest.js
@@ -0,0 +1,16 @@
+describe('GameCommand 입력값 테스트', () => {
+  test.each([['a'], ['1'], ['21'], ['1 ']])(
+    '(실패) 재시작/종료 입력값 확인',
+    (input) => {
+      const gameCommandException = new GameCommandException(input);
+      expect(() => gameCommandException.validate()).toThrow('[ERROR]');
+    }
+  );
+
+  test('(성공) 재시작/종료 입력값 확인', () => {
+    const input = '1';
+    const gameCommandException = new GameCommandException(input);
+
+    expect(() => gameCommandException.validate()).not.toThrow();
+  });
+});
