b7aedb2da0cd879a6f919eaa53b40e32d8751d80
turtle601 <turtle601@naver.com>
2022-12-04 21:51:32 +0900

    feat(BaseBallInputException) 숫자 입력 중복 체크 추가

--- src/validation/BaseBallInputException.js
+++ src/validation/BaseBallInputException.js
@@ -7,8 +7,16 @@ class BaseBallInputException {
     this.#input = input;
   }
 
+  #isOverlap() {
+    return new Set(this.#input.split('')).size === this.#input.length;
+  }
+
+  #isNumber() {
+    return REGEX.baseballInput.test(this.#input);
+  }
+
   validate() {
-    if (!REGEX.baseballInput.test(this.#input)) {
+    if (!(this.#isOverlap() && this.#isNumber())) {
       throw new Error(ERROR_MESSAGE.baseballInput);
     }
   }
