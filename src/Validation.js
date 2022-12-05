ebaba101ebea78345d4dd14dd74d2088f62eb7cc
daal2quf <daal2quf@gmail.com>
2022-12-05 23:05:03 +0900

    시간초과

--- src/Validation.js
+++ src/Validation.js
@@ -1,6 +1,6 @@
 const Validation = {
-  userInput(userInputNumbers) {
-    const regExp = new RegExp(/^[1-9]{1,3}$/gm);
+  userInputOfGameNumbers(userInputNumbers) {
+    const regExp = /^[1-9]{1,3}$/gm;
     if (userInputNumbers.length !== 3) {
       throw new Error('입력가능한 숫자의 길이는 3자리 입니다.');
     }
@@ -9,6 +9,11 @@ const Validation = {
     }
     if (userInputNumbers.length !== [...new Set(userInputNumbers)].length) {
       throw new Error('중복된 숫자는 불가합니다');
+    }
+  },
+  userInputRetryNumber(userInputNumber) {
+    if (userInputNumber !== '1' && userInputNumber !== '2') {
+      throw new Error('재시작 및 종료 입력은 1 과 2 만 가능합니다.');
     }
   },
 };
