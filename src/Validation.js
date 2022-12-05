315888e4429eea6388aa0f6e06d33359d8a0e677
daal2quf <daal2quf@gmail.com>
2022-12-05 22:09:22 +0900

    fix(Validation): 정규표현식을 이용한 예외처리 기능 오류 수정
    change:
    regExp.test 앞에 논리부정연산자를 붙이지않아 정상작동하지 않던 예외처리 수정

--- src/Validation.js
+++ src/Validation.js
@@ -2,13 +2,13 @@ const Validation = {
   userInput(userInputNumbers) {
     const regExp = new RegExp(/^[1-9]{1,3}$/gm);
     if (userInputNumbers.length !== 3) {
-      throw new Error('서로 다른 세자리 숫자를 입력해주세요');
+      throw new Error('입력가능한 숫자의 길이는 3자리 입니다.');
+    }
+    if (!regExp.test(userInputNumbers)) {
+      throw new Error('숫자만 입력해주세요');
     }
     if (userInputNumbers.length !== [...new Set(userInputNumbers)].length) {
       throw new Error('중복된 숫자는 불가합니다');
-    }
-    if (regExp.test(userInputNumbers)) {
-      throw new Error('숫자만 입력해주세요');
     }
   },
 };
