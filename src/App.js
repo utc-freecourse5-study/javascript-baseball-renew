c586acebee839eca527b316ee721ce92a626280d
woo-jk <201702042@o.cnu.ac.kr>
2022-12-11 17:46:14 +0900

    feat: App 클래스 tryValidator 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+const NumberMaker = require("./NumberMaker");
+const Validator = require("./Validator");
+const InputView = require("./view/InputView");
+const OutputView = require("./view/OutputView");
+
 class App {
-  play() {}
+  #computerNumbers;
+
+  play() {
+    this.#computerNumbers = NumberMaker.makeRandomNumber();
+    this.requestBaseBallNumber();
+  }
+
+  requestBaseBallNumber() {
+    InputView.readBaseBallNumbers((number) => {
+      this.tryValidate(Validator.validateNumber, number, this.requestBaseBallNumber);
+    });
+  }
+
+  tryValidate(validate, input, readLine) {
+    try {
+      validate(input);
+    } catch (error) {
+      OutputView.printErrorMessage(error);
+      readLine();
+    }
+  }
 }
 
 module.exports = App;
