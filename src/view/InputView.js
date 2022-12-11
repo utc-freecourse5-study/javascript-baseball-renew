5b049fed8350680cd2d080a57a7f302e7468cdeb
woo-jk <201702042@o.cnu.ac.kr>
2022-12-11 11:40:21 +0900

    feat: InputView readBaseBallNumbers 함수, readGameCommand 함수 제작

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,6 +1,12 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 const InputView = {
-  readBaseBallNumbers(output) {},
-  readGameCommand() {},
+  readBaseBallNumbers(callback) {
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", callback);
+  },
+  readGameCommand(callback) {
+    MissionUtils.Console.readLine("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.", callback);
+  },
 };
+
+module.exports = InputView;
