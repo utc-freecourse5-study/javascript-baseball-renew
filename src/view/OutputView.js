d3512b89b900b7260d294457b27e897364a22ea3
woo-jk <201702042@o.cnu.ac.kr>
2022-12-11 11:46:08 +0900

    feat: OutputView 객체 printGuessResult 함수, printFinalResult 함수 제작

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,8 +1,12 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 const OutputView = {
-  printGuessResult() {},
-  printFinalResult() {},
+  printGuessResult(result) {
+    MissionUtils.Console.print(result);
+  },
+  printFinalResult(count) {
+    MissionUtils.Console.print(`시도한 횟수: ${count}번`);
+  },
 };
 
 module.exports = OutputView;
