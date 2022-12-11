2300d914d17213df503f8f8d17f26d8703cfc12f
woo-jk <201702042@o.cnu.ac.kr>
2022-12-11 11:48:12 +0900

    feat: OutputView printOpening(), printEnding() 함수 제작

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,9 +1,18 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 const OutputView = {
+  printOpening() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  },
+
+  printEnding() {
+    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+  },
+
   printGuessResult(result) {
     MissionUtils.Console.print(result);
   },
+
   printFinalResult(count) {
     MissionUtils.Console.print(`시도한 횟수: ${count}번`);
   },
